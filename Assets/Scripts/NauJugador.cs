5f32e7c5bf5701de71af7a86f6ebf258b90cddd1
OscarDeDiegoSerrano <oscarde_diego@ceroca.cat>
2023-09-18 18:45:15 +0200

    Limits pantalla per la NauJugador

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -18,15 +18,33 @@ public class NauJugador : MonoBehaviour
         //GetAxis acelera poco a poco
         //GetAxisRaw de golpe
         float direccioHorizontal = Input.GetAxisRaw("Horizontal");
+        float direccioVertical = Input.GetAxisRaw("Vertical");
         //Para hacer un println
         //Debug.Log(direccioHorizontal);
 
-        Vector2 direccioIndicada = new Vector2(direccioHorizontal, 0f);
+        Vector2 direccioIndicada = new Vector2(direccioHorizontal, direccioVertical).normalized;
+
+        SpriteRenderer SpriteRenderer = GetComponent<SpriteRenderer>();
+        float anchura = SpriteRenderer.bounds.size.x/2;
+        float altura = SpriteRenderer.bounds.size.y/2;
+
+        //orthographicSize es la distancia desde el centro de la pantalla al borde.
+        //Camera.main.aspect devuelve cuanto mas de anchura hay respecto a la altura ya que no todas las pantallas tienen la altura = a la anchura. R = ANCHURA / ALTURA.
+        float limitEsquerraX = -Camera.main.orthographicSize * Camera.main.aspect + anchura;
+        float limitDretaX = Camera.main.orthographicSize * Camera.main.aspect - anchura;
+
+        float limitSuperiorY = Camera.main.orthographicSize - altura;
+        float limitInferiorY = -Camera.main.orthographicSize + altura;
 
         //Ens retorna la posició actual de la nau
         Vector2 novaPos = transform.position;
-        novaPos += direccioIndicada * _velNau * Time.deltaTime; 
+        novaPos += direccioIndicada * _velNau * Time.deltaTime;
         //Time.deltaTime fa que el joc vagi en la mateixa velocitat en ordinadors diferents.
+
+        novaPos.x = Mathf.Clamp(novaPos.x, limitEsquerraX, limitDretaX);
+        novaPos.y = Mathf.Clamp(novaPos.y, limitInferiorY, limitSuperiorY);
+
+        Debug.Log(novaPos.x);
 
         transform.position = novaPos;
     }
